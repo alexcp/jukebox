d55f2f30989fdf55b0e7b40771d1a4d80a0840e3
Alexandre Croteau-Pothier <alex@alexcp.com>
2015-04-21 15:02:36 -0400

    fix caching bug with the search page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @q = Concert.ransack
+    @concerts = Concert.includes(:venue, artists: :track).where("date >= ?", Date.today).order(date: :asc).group_by{|x| x.date.strftime("%B")}
+  end
+
+  def search
     @q = Concert.ransack(params[:q])
     @concerts = @q.result.includes(:venue, artists: :track).where("date >= ?", Date.today).order(date: :asc).group_by{|x| x.date.strftime("%B")}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists, only: :show
   resources :venues, only: :show
-
+  get "search", to: 'home#search'
   root to: 'home#index'
 end
