ba128ea35db03cc26a1201b29cdbea5d2930ae6b
Alexandre Croteau-Pothier <alex@alexcp.com>
2015-04-21 13:00:19 -0400

    seperate past concerts in the venue show page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,8 @@
 class VenuesController < ApplicationController
   def show
     @venue = Venue.includes(concerts: :artists).find(params[:id])
+    @concerts = @venue.concerts
+    @past_concerts = @concerts.where("date < ?", Date.today)
+    @concerts = @concerts.where("date >= ?", Date.today)
   end
 end
