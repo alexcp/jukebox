d1912f87678defc405e113aef29594e400f8984f
Alexandre Croteau-Pothier <alex@alexcp.com>
2015-04-21 12:06:00 -0400

    eager load artist's track

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @q = Concert.ransack(params[:q])
-    @concerts = @q.result.includes(:artists, :venue).where("date >= ?", Date.today).order(date: :asc).group_by{|x| x.date.strftime("%B")}
+    @concerts = @q.result.includes(:venue, artists: :track).where("date >= ?", Date.today).order(date: :asc).group_by{|x| x.date.strftime("%B")}
   end
 end
