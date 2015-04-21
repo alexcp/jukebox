9d9fe80b5cda7640f4a8262113f691d8bbb64fe3
Alexandre Croteau-Pothier <alex@alexcp.com>
2015-04-21 12:24:46 -0400

    add basic russian doll caching

--- app/helpers/concerts_helper.rb
+++ app/helpers/concerts_helper.rb
@@ -7,4 +7,17 @@ module ConcertsHelper
     artwork_url = concert.artists.select{|x| x.artwork_url}.first.try(:artwork_url) || "noimage.png"
     image_tag(artwork_url).html_safe
   end
+
+  def cache_key_for_concert concert
+    concert.id
+    track_ids = concert.artists.map{ |x| x.track.try(:id) }
+    "concerts/#{concert.id}-#{track_ids}"
+  end
+
+  def cache_key_for_concerts
+    count          = Concert.count
+    track_count    = Track.count
+    max_updated_at = Concert.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "concerts/all-#{count}-#{max_updated_at}-#{track_count}"
+  end
 end
